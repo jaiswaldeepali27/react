120a1e9b1a1300364e57d6d09b195cdee2ac41b3
jaiswal-sakshi <jaiswaldeepali110@gmail.com>
2025-02-16 13:47:01 +0530

    counter props

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
+
 
 function App() {
 
@@ -28,6 +28,7 @@ function App() {
       <button onClick={addValue}>Add Value</button>
       <br />
       <button onClick={removeValue}>Remove Value</button>
+      <Card username="Deepali" btnText="click me"/>
     </>
   )
 }
